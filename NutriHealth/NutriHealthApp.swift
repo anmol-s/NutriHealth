01755a3d01fd0f553ea2334a41a90e49d268610b
Anmol Singh <anmols5@uci.edu>
2021-02-06 22:37:47 -0800

    init

---
+++ NutriHealth/NutriHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriHealthApp.swift
+//  NutriHealth
+//
+//  Created by Owner on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct NutriHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
