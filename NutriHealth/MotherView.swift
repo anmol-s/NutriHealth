579b7a0360714d5913f318b82f057ed8268c7f82
Yasvi Patel <patelya@uci.edu>
2021-02-17 22:51:55 -0800

    Mother view and Home view added

---
+++ NutriHealth/MotherView.swift
@@ -0,0 +1,20 @@
+//
+//  MotherView.swift
+//  NutriHealth
+//
+//  Created by apple on 2/17/21.
+//
+
+import SwiftUI
+
+struct MotherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MotherView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotherView()
+    }
+}
