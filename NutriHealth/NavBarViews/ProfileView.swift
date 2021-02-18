779c4c96c3cfb303ec7fa06a04110e42cb06e1a6
Yasvi Patel <patelya@uci.edu>
2021-02-18 00:33:00 -0800

    Added files for Report page and Profile Page under NavBarViews

---
+++ NutriHealth/NavBarViews/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  NutriHealth
+//
+//  Created by apple on 2/18/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
---
+++ NutriHealth/NavBarViews/ReportView.swift
@@ -0,0 +1,20 @@
+//
+//  ReportView.swift
+//  NutriHealth
+//
+//  Created by apple on 2/18/21.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReportView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportView()
+    }
+}
