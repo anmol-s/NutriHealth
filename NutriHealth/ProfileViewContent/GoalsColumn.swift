a2781e7d9aa0b5df3f3d395586a8b1cbf0268c67
Yasvi Patel <patelya@uci.edu>
2021-03-12 14:21:23 -0800

    Profile page pulled from db

--- NutriHealth/ProfileViewContent/GoalsColumn.swift
+++ NutriHealth/ProfileViewContent/GoalsColumn.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import Parse
 
 struct GoalsColumn: View {
+    @State var weight:Int = 0
+    @State var calories:Double = 0.0
+    @State var FitnessGoal:String = ""
     var body: some View {
         List{
-            GoalsColumnItem(goalName: "Weight", goalValue: "120 lbs")
-            GoalsColumnItem(goalName: "Calories", goalValue: "2300")
-            GoalsColumnItem(goalName: "Nutrients", goalValue: "Vitamin D, Protein")
+            GoalsColumnItem(goalName: "Fitness Goal", goalValue: FitnessGoal)
+            GoalsColumnItem(goalName: "Weight", goalValue: "\(weight) lbs")
+            GoalsColumnItem(goalName: "Calories", goalValue: "\(calories)")
+            
+        }
+        .onAppear(){
+            let query = PFQuery(className:"PersonalModel")
+            query.whereKey("user", equalTo:PFUser.current())
+            query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
+                if let error = error {
+                    // Log details of the failure
+                    print(error.localizedDescription)
+                } else if let objects = objects {
+                    // The find succeeded.
+                    print("Successfully retrieved \(objects.count) scores.")
+                    // Do something with the found objects
+                    for object in objects {
+                        print(object.objectId as Any)
+                        self.weight = object["weight"] as! Int
+                        self.FitnessGoal = object["fitnessGoals"] as! String
+                        self.calories = object["recommendedCalories"] as! Double
+                    }
+                }
+            }
         }
     }
 }
--- NutriHealth/ProfileViewContent/ProfileColumn.swift
+++ NutriHealth/ProfileViewContent/ProfileColumn.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import Parse
 
 struct ProfileColumn: View {
+    @State var age:String = ""
+    @State var gender:String = ""
+    @State var weight:Int = 0
+    @State var height:Int = 0
+    @State var activity: String = ""
+    
     var body: some View {
         List{
-            GoalsColumnItem(goalName: "Age", goalValue: "21")
-            GoalsColumnItem(goalName: "Weight", goalValue: "110 lbs")
-            GoalsColumnItem(goalName: "Height", goalValue: "5 ft 7 in")
-            GoalsColumnItem(goalName: "Active", goalValue: "Moderate")
-            GoalsColumnItem(goalName: "Dietary Restrictions", goalValue: "Vegeterian")
-            GoalsColumnItem(goalName: "Allergies", goalValue: "None")
+            GoalsColumnItem(goalName: "Age", goalValue: age)
+            GoalsColumnItem(goalName: "Gender", goalValue: gender)
+            GoalsColumnItem(goalName: "Weight", goalValue: "\(weight) lbs")
+            GoalsColumnItem(goalName: "Height", goalValue: "\(height) cm")
+            GoalsColumnItem(goalName: "Activity Level", goalValue: activity)
+        }
+        .onAppear(){
+            let query = PFQuery(className:"PersonalModel")
+            query.whereKey("user", equalTo:PFUser.current())
+            query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
+                if let error = error {
+                    // Log details of the failure
+                    print(error.localizedDescription)
+                } else if let objects = objects {
+                    // The find succeeded.
+                    print("Successfully retrieved \(objects.count) scores.")
+                    // Do something with the found objects
+                    for object in objects {
+                        print(object.objectId as Any)
+                        self.age = object["age"] as! String
+                        self.gender = object["gender"] as! String
+                        self.weight = object["weight"] as! Int
+                        self.height = object["height"] as! Int
+                        self.activity = object["activityLevel"] as! String
+                    }
+                }
+            }
         }
     }
 }
