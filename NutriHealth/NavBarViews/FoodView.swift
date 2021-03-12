8a2f44dd1c64a6e7237971d7c7230874dc1789c4
Anmol Singh <anmols5@uci.edu>
2021-03-12 04:16:56 -0800

    Added "Food" dashboard to the food page

--- NutriHealth/NavBarViews/FoodView.swift
+++ NutriHealth/NavBarViews/FoodView.swift
@@ -1,5 +1,5 @@
 //
-//  ReportView.swift
+//  FoodView.swift
 //  NutriHealth
 //
 //  Created by apple on 2/18/21.
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct FoodView: View {
+    @State var selectedView = 0
+    
+    init(){
+        UISegmentedControl.appearance().selectedSegmentTintColor = .systemBlue
+        UISegmentedControl.appearance().backgroundColor = UIColor.white
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+    }
+    
     var body: some View {
-        Text("Food Page")
+        VStack{
+            Rectangle()
+                .frame(height: 80)
+                .foregroundColor(.blue)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 2, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+            FoodPageTitle().offset(y:-41)
+            Spacer()
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
@@ -18,3 +33,11 @@ struct FoodView_Previews: PreviewProvider {
         FoodView()
     }
 }
+
+struct FoodPageTitle: View {
+    var body: some View {
+        Text("Food")
+            .font(.system(size: 18)).fontWeight(.semibold)
+    }
+}
+
